eee11e42b108e2020e30a64ab9e19958c4c9a93e
Iñaki Retegi <retegi84@gmail.com>
2022-10-04 20:55:49 +0200

    cambio en wsgi

--- webeuskodev/wsgi.py
+++ webeuskodev/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webretegi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webeuskodev.settings')
 
 application = get_wsgi_application()
